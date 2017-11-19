39621f25ee479aa7ee03c0d7bb59dff1a67a488f
timotheehatton <timothee.hatton@gmail.com>
2017-11-19 23:59:15 +0100

    :construction: Home structure

--- wp-content/themes/flinked/template/home/home-main.php
+++ wp-content/themes/flinked/template/home/home-main.php
@@ -4,19 +4,8 @@ $home_header = get_field($prefixe.'image');
 $home_upTitle = get_field($prefixe.'upTitle');
 $home_title = get_field($prefixe.'title');
 
-echo'home header';
-echo '</br>';
-
-echo $home_header;
-
-echo '</br>';
-
-echo $home_upTitle;
-
-echo '</br>';
-
-echo $home_title;
-
-echo '</br>';
-echo'--------------------------';
 ?>
+
+<img src="<?php echo $home_header; ?>" alt="">
+<h1 class="block__header__content__title"><?php echo $home_title; ?></h1>
+<h4 class="title__up"><?php echo $home_upTitle; ?></h4>
