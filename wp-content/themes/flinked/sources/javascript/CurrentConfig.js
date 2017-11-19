f5d69debe57b6827a992d711279e689fdee75cc1
alexbeno <alexbenoliel@gmail.com>
2017-11-19 22:49:37 +0100

    :art: improving structure of devis.js

--- wp-content/themes/flinked/sources/javascript/CurrentConfig.js
+++ wp-content/themes/flinked/sources/javascript/CurrentConfig.js
@@ -8,55 +8,65 @@ const CurrentConfig = {
   price: 0,
   listOfOption: {
     moteur_moteur: {
+      section: 'moteur',
       price: 0,
       name: null,
       slug: null,
     },
 
     exterieur_teintes_exterieures: {
+      section: 'teinte extérieure',
       price: 0,
       name: null,
       slug: null,
     },
     exterieur_jantes: {
+      section: 'jante',
       price: 0,
       name: null,
       slug: null,
     },
 
     interieur_interieur: {
+      section: 'interieur',
       price: 0,
       name: null,
       slug: null,
     },
 
     pack_pack: {
+      section: 'pack',
       price: 0,
       name: null,
       slug: null,
     },
 
     option_exterieur: {
+      section: 'option exterieur',
       price: 0,
       name: null,
       slug: null,
     },
     option_interieur: {
+      section: 'option interieur',
       price: 0,
       name: null,
       slug: null,
     },
     option_fonctionnel: {
+      section: 'option fonctionnel',
       price: 0,
       name: null,
       slug: null,
     },
     option_technologies: {
+      section: 'option technologies',
       price: 0,
       name: null,
       slug: null,
     },
     option_jantes: {
+      section: 'option jante',
       price: 0,
       name: null,
       slug: null,
--- wp-content/themes/flinked/sources/javascript/Devis.js
+++ wp-content/themes/flinked/sources/javascript/Devis.js
@@ -1,3 +1,5 @@
+import CurrentConfig from './CurrentConfig.js'
+
 class Devis
 {
     /**
@@ -10,17 +12,68 @@ class Devis
       this.backward = document.querySelector('.config__opacity')
     }
 
+    clean() {
+      let item = document.querySelectorAll('.devis_item-container');
+      for (let index = 0; index <item.length; index++) {
+        item[index].remove();
+        
+      }
+    }
+
+    fillConfig(section, name, price, slug) {
+      let item = {};
+      item.container = document.createElement('div')
+      item.section = document.createElement('p')
+      item.name = document.createElement('p')
+      item.price = document.createElement('p')
+
+      item.container.classList.add('devis_item-container')
+      item.name.classList.add('devis_item-container_item')
+      item.price.classList.add('devis_item-container_item')
+      item.section.classList.add('devis_item-container_item')
+
+      item.name.innerText = name
+      item.price.innerText = price
+      item.section.innerText = section
+
+      item.name.setAttribute('data-slug', slug);
+
+      this.main.appendChild(item.container)
+      item.container.appendChild(item.section)
+      item.container.appendChild(item.name)
+      item.container.appendChild(item.price)
+
+    }
+
+    getConfig() {
+      let config = CurrentConfig['listOfOption'];
+      let that = this;
+      for (let key in config) {
+
+        let section = config[key]['section'];
+        let name = config[key]['name'];
+        let price = config[key]['price'];
+        let slug = config[key]['slug'];
+
+        if(slug != null) {
+          that.fillConfig(section, name, price, slug)
+        }
+      }
+    }
+
     show() {
       let that = this;
       this.main.classList.add('config__pop--active')
       this.backward.classList.add('config__opacity--active')
       setTimeout(function(){
         that.closes();
+        that.getConfig()
       }, 200);
     }
     unshow() {
       this.main.classList.remove('config__pop--active')
       this.backward.classList.remove('config__opacity--active')
+      this.clean();
     }
     open() {
       let that = this;
