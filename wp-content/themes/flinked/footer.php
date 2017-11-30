23bcb71f0170dabdca20fe22bb4999d2c93c9b4e
timotheehatton <timothee.hatton@gmail.com>
2017-11-30 22:21:49 +0100

    :sparkles: Footer

--- wp-content/themes/flinked/footer.php
+++ wp-content/themes/flinked/footer.php
@@ -1,5 +1,37 @@
 
         <!-- Execution de la fonction wp_footer() obligatoire ! -->
         <?php wp_footer();  ?>
+        <footer class="footer">
+            <div class="footer__item">
+                <a href="<?php echo get_option('home'); ?>/" ><img class="footer__item__logo" src="<?=IMAGES_URL?>/bmw.png"/></a>
+            </div>
+            <div class="footer__item">
+                <span class="footer__item__title">Services</span>
+                <a class="footer__item__link" href="">Bmw Group</a>
+                <a class="footer__item__link" href="">Nos sites</a>
+                <a class="footer__item__link" href="">Nos concession</a>
+                <a class="footer__item__link" href="">Certification ISO 9001</a>
+            </div>
+            <div class="footer__item">
+                <span class="footer__item__title">Services</span>
+                <a class="footer__item__link" href="">Bmw Group</a>
+                <a class="footer__item__link" href="">Nos sites</a>
+                <a class="footer__item__link" href="">Nos concession</a>
+                <a class="footer__item__link" href="">Certification ISO 9001</a>
+            </div>
+            <div class="footer__item">
+                <span class="footer__item__title">Services</span>
+                <a class="footer__item__link" href="">Bmw Group</a>
+                <a class="footer__item__link" href="">Nos sites</a>
+                <a class="footer__item__link" href="">Nos concession</a>
+                <a class="footer__item__link" href="">Certification ISO 9001</a>
+            </div>
+            <div class="footer__item">
+                <a class="footer__item__social" href="">f</a>
+                <a class="footer__item__social" href="">t</a>
+                <a class="footer__item__social" href="">y</a>
+            </div>
+            <span class="footer__copyright">© BMW France 2017</span>
+        </footer>
     </body>
 </html>
