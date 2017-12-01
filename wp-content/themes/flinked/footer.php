4a269a27817fae084d150de415786671a9833848
alexbeno <alexbenoliel@gmail.com>
2017-12-01 01:37:42 +0100

    :sparkles: add dynamique footer

--- wp-content/themes/flinked/footer.php
+++ wp-content/themes/flinked/footer.php
@@ -1,37 +1,76 @@
+<?php
+    $QuiSommeNous_title = get_field('qui_sommes-nous--titre', 'option');
+    $conseil_title = get_field('conseil_d’achat--titre', 'option');
+    $service_title = get_field('services--titre', 'option');
 
-        <!-- Execution de la fonction wp_footer() obligatoire ! -->
-        <?php wp_footer();  ?>
+    $link_fb = get_field('url_facebook', 'option');
+    $link_twitter = get_field('url_twitter', 'option');
+    $link_youtube = get_field('url_youtube', 'option');
+?>
+
+<?php
+if( have_rows('repeater_field_name') ):
+    while ( have_rows('repeater_field_name') ) : the_row();
+?>
+    
+<?php
+        the_sub_field('sub_field_name');
+    endwhile;
+else :
+endif;
+
+?>
         <footer class="footer">
             <div class="footer__item">
                 <a href="<?php echo get_option('home'); ?>/" ><img class="footer__item__logo" src="<?=IMAGES_URL?>/bmw.png"/></a>
             </div>
             <div class="footer__item">
-                <span class="footer__item__title">Services</span>
-                <a class="footer__item__link" href="">Bmw Group</a>
-                <a class="footer__item__link" href="">Nos sites</a>
-                <a class="footer__item__link" href="">Nos concession</a>
-                <a class="footer__item__link" href="">Certification ISO 9001</a>
+                <span class="footer__item__title"><?= $QuiSommeNous_title ?></span>
+                <?php
+                if( have_rows('qui_sommes-nous--contenu', 'option') ):
+                    while ( have_rows('qui_sommes-nous--contenu', 'option') ) : the_row();
+                ?>
+                    <a class="footer__item__link" href="<?= the_sub_field('qui_sommes-nous--contenu--lien', 'option');?>" target="_blank"><?= the_sub_field('qui_sommes-nous--contenu--texte', 'option');?></a>
+                <?php
+                    endwhile;
+                else :
+                endif;
+                ?>
             </div>
             <div class="footer__item">
-                <span class="footer__item__title">Services</span>
-                <a class="footer__item__link" href="">Bmw Group</a>
-                <a class="footer__item__link" href="">Nos sites</a>
-                <a class="footer__item__link" href="">Nos concession</a>
-                <a class="footer__item__link" href="">Certification ISO 9001</a>
+                <span class="footer__item__title"><?= $conseil_title ?></span>
+                <?php
+                if( have_rows('conseil_d’achat--contenu', 'option') ):
+                    while ( have_rows('conseil_d’achat--contenu', 'option') ) : the_row();
+                ?>
+                    <a class="footer__item__link" href="<?= the_sub_field('conseil_d’achat--contenu--lien', 'option');?>" target="_blank"><?= the_sub_field('conseil_d’achat--contenu--texte', 'option');?></a>
+                <?php
+                    endwhile;
+                else :
+                endif;
+                ?>
             </div>
             <div class="footer__item">
-                <span class="footer__item__title">Services</span>
-                <a class="footer__item__link" href="">Bmw Group</a>
-                <a class="footer__item__link" href="">Nos sites</a>
-                <a class="footer__item__link" href="">Nos concession</a>
-                <a class="footer__item__link" href="">Certification ISO 9001</a>
+                <span class="footer__item__title"><?= $service_title ?></span>
+                <?php
+                if( have_rows('services--contenu', 'option') ):
+                    while ( have_rows('services--contenu', 'option') ) : the_row();
+                ?>
+                    <a class="footer__item__link" href="<?= the_sub_field('services--contenu--lien', 'option');?>" target="_blank"><?= the_sub_field('services--contenu--texte', 'option');?></a>
+                <?php
+                    endwhile;
+                else :
+                endif;
+                ?>
             </div>
             <div class="footer__item">
-                <a class="footer__item__social" href="">f</a>
-                <a class="footer__item__social" href="">t</a>
-                <a class="footer__item__social" href="">y</a>
+                <a class="footer__item__social" target="_blank" href="<?= $link_fb ?>">f</a>
+                <a class="footer__item__social" target="_blank" href="<?= $link_twitter ?>">t</a>
+                <a class="footer__item__social" target="_blank" href="<?= $link_youtube ?>">y</a>
             </div>
             <span class="footer__copyright">© BMW France 2017</span>
         </footer>
+        <!-- Execution de la fonction wp_footer() obligatoire ! -->
+        <?php wp_footer();  ?>
     </body>
 </html>
